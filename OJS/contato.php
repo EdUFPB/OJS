88f934bdfbca496377ce2038f8c1bbe8346097e2
Pedro Henrique <pedro.sousa@dce.ufpb.br>
2019-07-25 15:27:13 -0300

    Remoção de erros textuais nas páginas de dúvidas e contato

--- OJS/contato.php
+++ OJS/contato.php
@@ -6,9 +6,6 @@
         </section>
 			<section id="main" class="wrapper">
                     <div class="container">
-                        <h2>Contato</h2>
-                            <p>O Portal está vinculado à Editora da UFPB e é coordenado pelo Prof. Dr. Henry Poncio Cruz de Oliveira (DCI/CCSA/UFPB). <br>
-                                Utiliza o Open Journal Systems (OJS), desenvolvido pelo Public Knowleadge Project (PKP), que adota as inciativas de acesso aberto ao conhecimento. </p>
                         
                         <h2>Contato Principal</h2>
                                 <p> <strong>Telefone</strong> - (83) 3216 - 7147</p>
@@ -16,9 +13,9 @@
                         
                         <h2>Equipe</h2>
                                 <p>Coordenador do Portal: Henry Pôncio</p>
-                                <p>Bibliotecária - Ducumentalista: Fabiana França</p>
+                                <p>Bibliotecária - Documentalista: Fabiana França</p>
                                 <p>Estagiário: Lucas Aragão</p>
-                                <p>Estagiário: Pedro Henrrique</p>
+                                <p>Estagiário: Pedro Henrique</p>
     
                     </div>
                 </section>
