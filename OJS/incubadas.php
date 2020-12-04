2f51d89ad9b906f711db54f6d1c3b85c065ea985
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2020-12-04 16:31:19 -0300

    :fire: feat: New section "incubados" in the website

---
+++ OJS/incubadas.php
@@ -0,0 +1,189 @@
+<?php include 'header.html'; ?>
+<!-- One -->
+
+<section id="one" class="wrapper style1 special">
+  <div class="container">
+    <header class="major">
+      <h2><strong>Periódicos</strong></h2>
+      <p>Periódicos em <strong>DESTAQUE</strong></p>
+    </header>
+    <div class="row 150%">
+      <div class="4u 12u$(medium)">
+        <section class="box">
+          <img src="images/informacao_e_sociedade.png" alt="" class="imageBox">
+          <h4><strong>Informação e Sociedade</strong></h4>
+          <p> É publicada ininterruptamente desde 1991 quando foi criada pela UFPB.
+          </p>
+        </section>
+      </div>
+      <div class="4u 12u$(medium)">
+        <section class="box">
+          <img src="images/moringa.png" alt="" class="imageBox">
+          <h4><strong>Moringa</strong></h4>
+          <p> MORINGA é uma publicação semestral do Departamento de Artes Cênicas, vinculado ao Centro de
+            Comunicação. <br></p>
+        </section>
+      </div>
+      <div class="4u$ 12u$(medium)">
+        <section class="box">
+          <img src="images/juee1.png" alt="" class="imageBox">
+          <h4><strong>JUEE</strong></h4>
+          <p> Journal of Urban and Environmental Engineering (JUEE) provides a forum for original papers.</p>
+          </a>
+        </section>
+      </div>
+    </div>
+
+  </div>
+  <hr />
+</section>
+
+<!-- Main -->
+<section id="main" class="wrapper" style="padding-top: 0px;">
+  <div class="container">
+
+    <header class="major">
+      <h2><strong>Revistas Incubadas</strong></h2>
+      <h4> <strong>Confira abaixo a lista de periódicos recém criados e que não possuem edição.</strong> </h4>
+      <br>
+      <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Pesquise por periódicos">
+    </header>
+    <hr />
+  </div>
+
+
+  <!-- REVISTAS INICIO -->
+  <div class="container">
+
+    <ul id="myUL">
+      <li>
+        <br>
+        <div class="row">
+          <div class="col-md-3 col-lg-3">
+            <img class="float left" src="images/dsbr.png" alt="" style="width: 100%;margin-right:15px;">
+          </div>
+          <div class="col-md-9 col-lg-9">
+            <p class="text-justify">
+              <a href="https://periodicos.ufpb.br/index.php/dsbr/index">Data Science and Business Review</a>
+              <br>
+              <strong>Escopo: </strong>A revista <strong>Data Science and Business Review</strong> é um periódico
+              acadêmico criado emantido pelo Programa de Pós-Graduação em Ciência de Dados para Negócios daUFPB , com o
+              objetivo de disseminar conhecimentos em Ciência de Dados entrepesquisadores, profissionais,
+              empreendedores, gestores de organizações públicas eprivadas, e formuladores de políticas públicas.
+              <br>
+              <strong>e-ISSN: </strong>
+              <br>
+              <strong>Contato: </strong>pjacome@sti.ufpb.br
+              <br>
+              <br>
+              <a class="btn btn-outline-primary btn-sm" href="https://periodicos.ufpb.br/index.php/dsbr/index/">Acessar
+                Revista</a>
+              <a class="btn btn-outline-primary btn-sm"
+                href="https://periodicos.ufpb.br/index.php/dsbr/issue/current">Edições Disponíveis</a>
+            </p>
+            <hr>
+          </div>
+        </div>
+      </li>
+      <li>
+        <br>
+        <div class="row">
+          <div class="col-md-3 col-lg-3">
+            <img class="float left" src="images/cognens.png" alt="" style="width: 100%;margin-right:15px;">
+          </div>
+          <div class="col-md-9 col-lg-9">
+            <p class="text-justify">
+              <a href="https://periodicos.ufpb.br/index.php/cee/index">Cognição e Ensino</a>
+              <br>
+              <strong>Escopo: </strong>A revista <strong>Cognição e Ensino</strong> objetiva aglutinar estudos e
+              pesquisas interdisciplinares que tenham como foco as Ciências Cognitivas e suas contribuições para a
+              relação Ensino-Aprendizagem. Considera as possibilidades de manifestações políticas, culturais,
+              tecnológicas e linguísticas do cotidiano, as noções sobre inteligência e aprendizagem, e os múltiplos
+              debates sobre educação comparada, com destaque no desenvolvimento regional e nacional.
+              <br>
+              <strong>e-ISSN: </strong>
+              <br>
+              <strong>Contato: </strong>giulianacv@yahoo.com.br
+              <br>
+              <br>
+              <a class="btn btn-outline-primary btn-sm" href="https://periodicos.ufpb.br/index.php/cee/index/">Acessar
+                Revista</a>
+              <a class="btn btn-outline-primary btn-sm"
+                href="https://periodicos.ufpb.br/index.php/cee/issue/current">Edições Disponíveis</a>
+            </p>
+            <hr>
+          </div>
+        </div>
+      </li>
+      <li>
+        <br>
+        <div class="row">
+          <div class="col-md-3 col-lg-3">
+            <img class="float left" src="images/bjgfs.png" alt="" style="width: 100%;margin-right:15px;">
+          </div>
+          <div class="col-md-9 col-lg-9">
+            <p class="text-justify">
+              <a href="https://periodicos.ufpb.br/index.php/bjgfs/index">BRAZILIAN JOURNAL OF GASTRONOMY AND FOOD
+                SCIENCE</a>
+              <br>
+              <strong>Escopo: </strong>O <strong>BRAZILIAN JOURNAL OF GASTRONOMY AND FOOD SCIENCE</strong> é um veículo
+              de divulgação de artigos científicos com periodicidade semestral, editado pelo Departamento de Gastronomia
+              do Centro de Tecnologia e Desenvolvimento Regional (CTDR) da Universidade Federal da Paraíba (UFPB). O
+              referido jornal vem preencher a lacuna existente no país quanto à existência de periódicos científicos que
+              garantam, a efetiva divulgação da produção técnico-científica dos docentes e pesquisadores das áreas de
+              Gastronomia e Ciência de Alimentos no Brasil. Trata-se de uma Revista Técnico-Científica de ampla
+              circulação e com escopo diversificado envolvendo as áreas de Alta Gastronomia, Gastronomia Brasileira,
+              Confeitaria e Panificação, Memória e cultura, Ciência de Alimentos, Tecnologia de Alimentos.
+              <br>
+              <strong>e-ISSN: </strong>
+              <br>
+              <strong>Contato: </strong>ingridcdantas@hotmail.com
+              <br>
+              <br>
+              <a class="btn btn-outline-primary btn-sm" href="https://periodicos.ufpb.br/index.php/bjgfs/index/">Acessar
+                Revista</a>
+              <a class="btn btn-outline-primary btn-sm"
+                href="https://periodicos.ufpb.br/index.php/bjgfs/issue/current">Edições Disponíveis</a>
+            </p>
+            <hr>
+          </div>
+        </div>
+      </li>
+      <li>
+        <br>
+        <div class="row">
+          <div class="col-md-3 col-lg-3">
+            <img class="float left" src="images/literalmente.jpeg" alt="" style="width: 100%;margin-right:15px;">
+          </div>
+          <div class="col-md-9 col-lg-9">
+            <p class="text-justify">
+              <a href="https://periodicos.ufpb.br/index.php/rl/index">LiteralMente</a>
+              <br>
+              <strong>Escopo: </strong>A revista <strong>LiteralMENTE</strong> é uma publicação eletrônica do Grupo de
+              Pesquisa em
+              Literatura, Gênero e Psicanálise (LIGEPSI - UFPB). Publica artigos, ensaios e traduções que contemplem as
+              interlocuções da Literatura com a Psicanálise, com a Psicologia Analítica e coma Psicopatologia. Tem
+              periodicidade semestral (com edições em julho e dezembro).
+              <br>
+              <strong>e-ISSN: </strong>
+              <br>
+              <strong>Contato: </strong>henriquemais@gmail.com
+              <br>
+              <br>
+              <a class="btn btn-outline-primary btn-sm" href="https://periodicos.ufpb.br/index.php/rl/index/">Acessar
+                Revista</a>
+              <a class="btn btn-outline-primary btn-sm"
+                href="https://periodicos.ufpb.br/index.php/rl/issue/current">Edições Disponíveis</a>
+            </p>
+            <hr>
+          </div>
+        </div>
+      </li>
+    </ul>
+
+  </div>
+</section>
+<!-- Footer -->
+<?php 
+            include 'footer.html'
+            ?>
