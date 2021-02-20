a69f14ed7abbde25261d679b74d1d2be121f6a5f
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-02-20 12:44:57 -0300

    :fire: feat: "Quem somos" change to "Sobre"

---
+++ OJS/sobre.php
@@ -0,0 +1,95 @@
+<?php
+    include 'header.html'
+?>
+
+<!-- Main -->
+<section id="one" class="style1 special">
+    <hr>
+</section>
+<section class="pt-5 pb-4">
+    <div class="container">
+
+        <header class="major">
+            <h2 class="mb-2"><strong>Sobre o Portal</strong></h2>
+            <p class="mt-1">Um pouco da nossa história.</p>
+        </header>
+
+        <!-- Text -->
+        <section>
+            <p>O <strong> Portal de Periódicos da UFPB </strong> objetiva dar visibilidade à produção científica por
+                meio de revistas eletrônicas científicas elaboradas ou gerenciadas por pesquisadores vinculados à UFPB,
+                possibilitando o acesso e uso de informações científicas e tecnológicas, oferecendo suporte aos editores
+                de periódicos da Instituição, em atenção às determinações da Coordenação de Aperfeiçoamento de Pessoal
+                de Nível Superior (CAPES) e do Conselho Nacional de Desenvolvimento Científico e Tecnológico (CNPq) no
+                que tange a gestão de periódicos científicos.</p>
+
+            <p>O Portal de Periódicos Científicos Eletrônicos da UFPB foi inaugurado em 18 de maio de 2006, sendo um dos
+                primeiros do gênero em Universidades Federais brasileiras. Foi viabilizado por meio de uma parceria
+                entre a Pró-Reitoria de Pesquisa e Pós-Graduação da UFPB e o Programa de Pós-Graduação em Ciência da
+                Informação da UFPB (PPGCI/UFPB).</p>
+
+            <p>O Portal está vinculado à Editora da UFPB e utiliza o Open Journal Systems (OJS), desenvolvido pelo
+                Public Knowledge Project (PKP), que adota as iniciativas de acesso aberto ao conhecimento.</p>
+        </section>
+
+        <section>
+            <header class="major mt-2">
+                <hr>
+                <h2 class="mb-2"><strong>Serviços</strong></h2>
+                <p class="mt-1">Serviços ofertados pela equipe.</p>
+            </header>
+            <p><strong>O Portal de Periódicos da UFPB tem como princípio orientar e prestar auxílio aos editores nos
+                    seguintes serviços:</strong></p>
+            <ul>
+                <li>Orientação aos editores quanto ao credenciamento no Portal;</li>
+                <li>Auxílio e orientação aos editores quanto à indexação em bases de dados;</li>
+                <li>Auxílio e orientação aos editores quanto à utilização do sistema de gerenciamento do Portal na
+                    inserção de números retrospectivos;</li>
+                <li>Capacitação e prestação de suporte para treinamentos referente a plataforma Open Journal Systems
+                    (OJS) para os editores e equipe técnica;</li>
+                <li>Orientação aos editores quanto às ética, boas práticas e normas editoriais para publicações
+                    periódicas;</li>
+                <li>Orientação sobre o surgimento de fontes e recursos de financiamento (editais);</li>
+                <li>Orientação e validação do Digital Object Identifier (DOI);</li>
+                <li>Orientação sobre utilização e abertura do registro ORCID (Open Researcher and Contributor ID);</li>
+                <li>Orientação aos editores da obtenção da numeração do ISSN junto ao IBICT;</li>
+                <li>Inclusão do periódico na Rede de Serviços de Preservação Digital (CARINIANA) do IBICT;</li>
+                <li>Incubação de periódicos emergentes que precisam obter os requisitos para inclusão no Portal;</li>
+            </ul>
+        </section>
+        <!-- Two -->
+        <section class="style2 special">
+            <hr>
+            <div class="container mt-2">
+                <header class="major">
+                    <h2 class="mb-2"><strong>Equipe</strong></h2>
+                    <p class="mt-2">Conheça a equipe do Portal de Periódicos</p>
+                </header>
+                <section class="profiles mb-5" id="teamSection">
+                    <div class="row justify-content-center">
+                        <section class="3u 6u(medium) 12u$(xsmall) profile">
+                            <img src="https://via.placeholder.com/100" alt="" width="100px" height="100px" />
+                            <p>Cassandra Campos</p>
+                            <h5 style="font-size: 15px"><strong>Coordenadora</strong></h5>
+                        </section>
+                        <section class="3u 6u$(medium) 12u$(xsmall) profile">
+                            <img src="images/fabi.png" alt="" width="100px" height="100px" />
+                            <p>Fabiana França</p>
+                            <h5 h5 style="font-size: 15px"><strong>Bibliotecária - Documentalista</strong></h5>
+                        </section>
+                        <section class="3u 6u$(medium) 12u$(xsmall) profile">
+                            <img src="images/pedro4.jpg" alt="" width="100px" height="100px" />
+                            <p>Pedro Henrique</p>
+                            <h5 h5 style="font-size: 15px"><strong>Estágio - Sistemas de Informação</strong></h5>
+                        </section>
+                    </div>
+                </section>
+            </div>
+        </section>
+    </div>
+
+</section>
+
+<?php
+            include 'footer.html';
+        ?>
