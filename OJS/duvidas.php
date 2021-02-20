853f41311d13bd2077f5eba6bf505e0591232ca9
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-02-20 14:18:49 -0300

    :fire: feat: "Dúvidas" DONE

--- OJS/duvidas.php
+++ OJS/duvidas.php
@@ -3,28 +3,139 @@ include 'header.html'
 ?>
 
 <!-- Main -->
-<section id="one" class="wrapper style1 special">
+<section id="one" class="style1 special">
+    <hr>
 </section>
-<section id="main" class="wrapper">
+<section class="pt-5">
     <div class="container">
-        <h2>Criação de Periódicos</h2>
-        <p>Para criar uma revista no Portal de Periódicos da UFPB é necessário destinar à Coordenação do Portal dois
-            documentos: </p>
-        <p>a) Documento que vincula a Revista a um Departamento ou programa de Pós-Graduação ou Grupo de Pesquisa
-            Certificado pelo CNPq. </p>
-        <p>b) Um projeto descritivo contendo:</p>
-        <ul>
-            <li>Nome da Revista</li>
-            <li>Logo da Revista</li>
-            <li>Equipe Editorial:</li>
-            <p> - Editores (Quem responde pela revista) <br>
-                - Comissão Editorial (Grupo pequeno de pesquisadores que apoiam os editores) <br>
-                - Conselho Consultivo (Grupo de pesquisadores/avaliadores da Revista) <br>
-                - Suporte (Quem resolverá os problemas técnicos da revista) </p>
-            <li>Foco e Escopo</li>
-            <li>Periodicidade</li>
-            <li>Diretrizes para autores (de acordo com as normas da ABNT) </li>
-        </ul>
+        <header class="major">
+            <h2 class="mb-2"><strong>FAQ - Dúvidas Frequentes</strong></h2>
+            <p class="mt-1">Aqui responderemos dúvidas frequentes para facilitar o uso dos nossos serviços.</p>
+        </header>
+
+    </div>
+</section>
+<section class="accordion-section clearfix mb-5" aria-label="Question Accordions">
+    <div class="container">
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+                <div class="panel-heading p-3 mb-3" role="tab" id="heading0">
+                    <h4 class="panel-title">
+                        <a class="collapsed text-center" role="button" title="" data-toggle="collapse"
+                            data-parent="#accordion" href="#collapse0" aria-expanded="true" aria-controls="collapse0">
+                            <strong>Como crio/hospedo minha revista no Portal?</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse0" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading0">
+                    <div class="panel-body px-3 mb-4 text-justify pl-5 pr-5">
+                        <p>Para criar uma revista no Portal de Periódicos da UFPB é necessário
+                            destinar à
+                            Coordenação do Portal
+                            dois documentos:
+                        </p>
+                        <p><strong>a)</strong> Documento que vincula a Revista a um Departamento ou programa de
+                            Pós-Graduação ou
+                            Grupo de
+                            Pesquisa Certificado pelo CNPq. </p>
+                        <p><strong>b)</strong> Um projeto descritivo contendo:</p>
+                        <ul>
+                            <li>Nome da Revista</li>
+                            <li>Logo da Revista</li>
+                            <li>Equipe Editorial: <br>
+                                - Editores (Quem responde pela revista) <br>
+                                - Comissão Editorial (Grupo pequeno de pesquisadores que apoiam os editores) <br>
+                                - Conselho Consultivo (Grupo de pesquisadores/avaliadores da Revista) <br>
+                                - Suporte (Quem resolverá os problemas técnicos da revista)
+                            </li>
+                            <li>Foco e Escopo</li>
+                            <li>Periodicidade</li>
+                            <li>Diretrizes para autores (de acordo com as normas da ABNT) </li>
+                            <li><strong><a href="fluxograma.html">Sobre esse sistema de Publicação</a></strong></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading p-3 mb-3" role="tab" id="heading1">
+                    <h4 class="panel-title">
+                        <a class="collapsed text-center" role="button" title="" data-toggle="collapse"
+                            data-parent="#accordion" href="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                            <strong>Como me cadastrar no Portal?</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading1">
+                    <div class="panel-body px-3 mb-4 text-justify pl-5 pr-5">
+                        <p class="text-justify">Acesse o botão "Acesso" no canto superior direito ou vá diretamente
+                            neste <a href="https://periodicos.ufpb.br/index.php/index/user/register?source=">LINK</a> ,
+                            onde será
+                            necessário repassar informações básicas como Nome, Email, Instituição, País, etc.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading p-3 mb-3" role="tab" id="heading2">
+                    <h4 class="panel-title">
+                        <a class="collapsed text-center" role="button" title="" data-toggle="collapse"
+                            data-parent="#accordion" href="#collapse2" aria-expanded="true" aria-controls="collapse2">
+                            <strong>Meu artigo foi avaliado?</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading2">
+                    <div class="panel-body px-3 mb-4 text-justify pl-5 pr-5">
+                        <p class="text-justify">Para acompanhar o status de seu artigo/manuscrito será necessário estar sempre atento ao seu
+                            email cadastrado no sistema, ou sempre observar o andamento do fluxo pelo sistema.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading p-3 mb-3" role="tab" id="heading3">
+                    <h4 class="panel-title">
+                        <a class="collapsed text-center" role="button" title="" data-toggle="collapse" data-parent="#accordion"
+                            href="#collapse3" aria-expanded="true" aria-controls="collapse3">
+                            <strong>Como entrar em contato?</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading3">
+                    <div class="panel-body px-3 mb-4 text-justify pl-5 pr-5">
+                        <p class="text-justify">
+                            Entre em contato conosco por email: <a href="mailto:periodicos.ufpb@gmail.com">periodicos.ufpb@gmail.com</a>
+                            <br>
+                            Ou pelo telefone: <a href="tel:+558432167147">(83) 3216 - 7147</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading p-3 mb-3" role="tab" id="heading4">
+                    <h4 class="panel-title">
+                        <a class="collapsed text-center" role="button" title="" data-toggle="collapse" data-parent="#accordion"
+                            href="#collapse4" aria-expanded="true" aria-controls="collapse4">
+                            <strong>Qual a equipe atual?</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading4">
+                    <div class="panel-body px-3 mb-4 text-justify pl-5 pr-5">
+                        <p class="text-justify">
+                            Nossa equipe é composta por:
+                            <br>
+                            Cassandra Campos - <strong>Coordenadora do Portal</strong>
+                            <br>
+                            Fabiana França - <strong>Bibliotecária - Documentalista</strong>
+                            <br>
+                            Pedro Henrique - <strong>Estagiário - Sistemas de Informação</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 <!-- Footer -->
