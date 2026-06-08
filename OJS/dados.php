b8a04c19cc0f0d0c60000ff37d5decea915453e1
anarobertamota <anarobertamota@gmail.com>
2026-06-08 11:50:14 -0300

    Adiciona dados.php

---
+++ OJS/dados.php
@@ -0,0 +1,484 @@
+<?php
+$revistas = [
+
+    // ==================== CORRENTES ====================
+    [
+        "nome" => "Informação & Sociedade: Estudos",
+        "link" => "https://periodicos.ufpb.br/index.php/ies",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Moringa",
+        "link" => "https://periodicos.ufpb.br/index.php/moringa",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Aufklärung",
+        "link" => "https://periodicos.ufpb.br/index.php/arf",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Culturas Midiáticas",
+        "link" => "https://periodicos.ufpb.br/index.php/cm",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Gaia Scientia",
+        "link" => "https://periodicos.ufpb.br/index.php/gaia",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Journal of Urban and Environmental Engineering",
+        "link" => "https://periodicos.ufpb.br/index.php/juee",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Perspectivas em Gestão & Conhecimento",
+        "link" => "https://periodicos.ufpb.br/index.php/pgc",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Prim@ Facie - Direito, História e Política",
+        "link" => "https://periodicos.ufpb.br/index.php/primafacie",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Problemata",
+        "link" => "https://periodicos.ufpb.br/index.php/problemata",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "PROLÍNGUA",
+        "link" => "https://periodicos.ufpb.br/index.php/prolingua",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Ártemis",
+        "link" => "https://periodicos.ufpb.br/index.php/artemis",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Espaço do Currículo",
+        "link" => "https://periodicos.ufpb.br/index.php/rec",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Graphos",
+        "link" => "https://periodicos.ufpb.br/index.php/graphos",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Lugares de Educação",
+        "link" => "https://periodicos.ufpb.br/index.php/rle",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista de Ciências Sociais - Política & Trabalho",
+        "link" => "https://periodicos.ufpb.br/index.php/politicaetrabalho",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Religare: Revista do Programa de Pós-Graduação em Ciências das Religiões da UFPB",
+        "link" => "https://periodicos.ufpb.br/index.php/religare",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Sæculum – Revista de História",
+        "link" => "https://periodicos.ufpb.br/index.php/srh",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "DLCV - Língua, Linguística & Literatura",
+        "link" => "https://periodicos.ufpb.br/index.php/dclv",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "OKARA: Geografia em debate",
+        "link" => "https://periodicos.ufpb.br/index.php/okara",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista da ABET",
+        "link" => "https://periodicos.ufpb.br/index.php/abet",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Temas em Educação",
+        "link" => "https://periodicos.ufpb.br/index.php/rteo",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Teoria e Prática em Administração",
+        "link" => "https://periodicos.ufpb.br/index.php/tpa",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Archeion Online",
+        "link" => "https://periodicos.ufpb.br/index.php/archeion",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Biblionline",
+        "link" => "https://periodicos.ufpb.br/index.php/biblio",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Cadernos Imbondeiro",
+        "link" => "https://periodicos.ufpb.br/index.php/ci",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Brasileira de Ciências da Saúde",
+        "link" => "https://periodicos.ufpb.br/index.php/rbcs",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Economia e Desenvolvimento",
+        "link" => "https://periodicos.ufpb.br/index.php/economia",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Evidenciação Contábil & Finanças",
+        "link" => "https://periodicos.ufpb.br/index.php/recfin",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Nordestina de Biologia",
+        "link" => "https://periodicos.ufpb.br/index.php/revnebio",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Cadernos do LOGEPA",
+        "link" => "https://periodicos.ufpb.br/index.php/logepa",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Claves",
+        "link" => "https://periodicos.ufpb.br/index.php/claves",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Cultura e Tradução",
+        "link" => "https://periodicos.ufpb.br/index.php/ct",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Diversidade Religiosa",
+        "link" => "https://periodicos.ufpb.br/index.php/dr",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Agropecuária Técnica",
+        "link" => "https://periodicos.ufpb.br/index.php/at",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Informação & Tecnologia",
+        "link" => "https://periodicos.ufpb.br/index.php/itec",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Letr@ Viv@",
+        "link" => "https://periodicos.ufpb.br/index.php/lv",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Logos & Existência",
+        "link" => "https://periodicos.ufpb.br/index.php/le",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Cultura Oriental",
+        "link" => "https://periodicos.ufpb.br/index.php/co",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Brasileira de Políticas Públicas e Internacionais - RPPI",
+        "link" => "https://periodicos.ufpb.br/index.php/rppi",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista de Iniciação Científica em Odontologia - RevICO",
+        "link" => "https://periodicos.ufpb.br/index.php/revico",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista de Iniciação Científica em Relações Internacionais",
+        "link" => "https://periodicos.ufpb.br/index.php/ricri",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Paraibana de História",
+        "link" => "https://periodicos.ufpb.br/index.php/rph",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Gestão & Aprendizagem",
+        "link" => "https://periodicos.ufpb.br/index.php/mpgoa",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Temática",
+        "link" => "https://periodicos.ufpb.br/index.php/tematica",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "ÂNCORA - Revista Latino-americana de Jornalismo",
+        "link" => "https://periodicos.ufpb.br/index.php/ancora",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Letras & Ideias",
+        "link" => "https://periodicos.ufpb.br/index.php/letraseideias",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Educare",
+        "link" => "https://periodicos.ufpb.br/index.php/educare",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Medicina & Pesquisa",
+        "link" => "https://periodicos.ufpb.br/index.php/rmp",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Comunicações em Informática",
+        "link" => "https://periodicos.ufpb.br/index.php/cei",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Discurso & Imagem Visual em Educação",
+        "link" => "https://periodicos.ufpb.br/index.php/rdive",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Áltera Revista de Antropologia",
+        "link" => "https://periodicos.ufpb.br/index.php/altera",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Científica de Produção Animal",
+        "link" => "https://periodicos.ufpb.br/index.php/rcpa",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Scandia Journal of Medieval Norse Studies",
+        "link" => "https://periodicos.ufpb.br/index.php/scandia",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "The Brazilian Trombone Association Journal",
+        "link" => "https://periodicos.ufpb.br/index.php/btaj",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista PetrART",
+        "link" => "https://periodicos.ufpb.br/index.php/petrart",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "CAOS – Revista Eletrônica de Ciências Sociais",
+        "link" => "https://periodicos.ufpb.br/index.php/caos",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Revista Abordagens",
+        "link" => "https://periodicos.ufpb.br/index.php/rappgs",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Prosppectus",
+        "link" => "https://periodicos.ufpb.br/index.php/prosp/index",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "LiteralMente",
+        "link" => "https://periodicos.ufpb.br/index.php/rl/index",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Direitos Humanos e Transdisciplinaridade",
+        "link" => "https://periodicos.ufpb.br/index.php/dht/index",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Ratio Iuris",
+        "link" => "https://periodicos.ufpb.br/index.php/rri/index",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Benjaminiana - Revista de Estudos em Tradução e Imagem",
+        "link" => "https://periodicos.ufpb.br/index.php/breti/index",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Sanhauá",
+        "link" => "https://periodicos.ufpb.br/index.php/snh/index",
+        "colecao" => "correntes"
+    ],
+    [
+        "nome" => "Sudamérica: Revista Internacional de Direitos Humanos",
+        "link" => "https://periodicos.ufpb.br/index.php/sda/index",
+        "colecao" => "correntes"
+    ],
+
+    // ==================== INCUBADOS ====================
+    [
+        "nome" => "Revista InterCulturas",
+        "link" => "https://periodicos.ufpb.br/index.php/rics/index",
+        "colecao" => "incubados"
+    ],
+    [
+        "nome" => "Textos NDIHR",
+        "link" => "https://periodicos.ufpb.br/index.php/ndihr/index",
+        "colecao" => "incubados"
+    ],
+    [
+        "nome" => "Ostium Solis",
+        "link" => "https://periodicos.ufpb.br/index.php/ros/index",
+        "colecao" => "incubados"
+    ],
+    [
+        "nome" => "PRÁXIS Educação e Diversidade",
+        "link" => "https://periodicos.ufpb.br/index.php/prx/index",
+        "colecao" => "incubados"
+    ],
+    [
+        "nome" => "Data Science and Business Review",
+        "link" => "https://periodicos.ufpb.br/index.php/dsbr/index",
+        "colecao" => "incubados"
+    ],
+    [
+        "nome" => "Cognição e Ensino",
+        "link" => "https://periodicos.ufpb.br/index.php/cee/index",
+        "colecao" => "incubados"
+    ],
+    [
+        "nome" => "Brazilian Journal of Gastronomy and Food Science",
+        "link" => "https://periodicos.ufpb.br/index.php/bjgfs/index",
+        "colecao" => "incubados"
+    ],
+    [
+        "nome" => "RILAER - Revista Interinstitucional Latino-americana de Ensino Religioso",
+        "link" => "https://periodicos.ufpb.br/index.php/rilaer/index",
+        "colecao" => "incubados"
+    ],
+    [
+        "nome" => "Perg@minho - Revista Discente de História",
+        "link" => "https://periodicos.ufpb.br/index.php/perg/index",
+        "colecao" => "incubados"
+    ],
+
+    // ==================== NÃO CORRENTES ====================
+    [
+        "nome" => "Acta Semiótica et Lingvistica",
+        "link" => "https://periodicos.ufpb.br/index.php/actas",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Métodos e Pesquisa em Administração",
+        "link" => "https://periodicos.ufpb.br/index.php/mepad",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Pesquisa Brasileira em Ciência da Informação e Biblioteconomia",
+        "link" => "https://periodicos.ufpb.br/index.php/pbcib",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Revista Gênero & Direito",
+        "link" => "https://periodicos.ufpb.br/index.php/ged",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "HIDROGEOGRAFIA: Geografias das águas",
+        "link" => "https://periodicos.ufpb.br/index.php/hidrogeografias",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "CIAB - Ciência da Informação em Aberto",
+        "link" => "https://periodicos.ufpb.br/index.php/ciab",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "VERBA JURIS - Anuário da Pós-Graduação em Direito",
+        "link" => "https://periodicos.ufpb.br/index.php/vj",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Ensaios",
+        "link" => "https://periodicos.ufpb.br/index.php/ensaios",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Revista Estudos Geoambientais",
+        "link" => "https://periodicos.ufpb.br/index.php/geo",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Turis Nostrum",
+        "link" => "https://periodicos.ufpb.br/index.php/tn",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Teoria Política & Social",
+        "link" => "https://periodicos.ufpb.br/index.php/tps",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Revista de Arqueologia",
+        "link" => "https://periodicos.ufpb.br/index.php/ra",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "AUTOGESTÃO: Economia dos Trabalhadores & Educação Popular",
+        "link" => "https://periodicos.ufpb.br/index.php/autogestao",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Revista Eletrônica Extensão Cidadã",
+        "link" => "https://periodicos.ufpb.br/index.php/extensaocidada",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Revista Pesquisas em Ciências Médicas",
+        "link" => "https://periodicos.ufpb.br/ojs2/index.php/rpcm",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Arquivo & Sociedade: Estudos",
+        "link" => "https://periodicos.ufpb.br/index.php/aes",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Revista Sublimação",
+        "link" => "https://periodicos.ufpb.br/index.php/sublimacao",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Humanare - Perspectivas e desafios para a humanização do Direito",
+        "link" => "https://periodicos.ufpb.br/index.php/ridcc",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Comunicação Audiovisual",
+        "link" => "https://periodicos.ufpb.br/index.php/palavrar",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Esfera Pública",
+        "link" => "",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Periódico Escola do Curso de Biblioteconomia",
+        "link" => "",
+        "colecao" => "nao-correntes"
+    ],
+    [
+        "nome" => "Revista de Educação e Conhecimento em Administração",
+        "link" => "",
+        "colecao" => "nao-correntes"
+    ],
+];
